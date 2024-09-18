5cddff8910d45efca6acbc9bb4ba28c2c9abb609
ranger <1648186705@qq.com>
2024-09-18 18:54:36 +0800

    perf:post content

---
+++ src/components/Post/PostComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <el-card>
+    <template #header>
+      <div class="card-header">
+        <span>Card name</span>
+      </div>
+    </template>
+    <div>Card content</div>
+    <template #footer>阅读原文 -- 百度百科</template>
+  </el-card>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,11 +1,19 @@
 <script setup>
 
+import PostComponent from "@/components/Post/PostComponent.vue";
 </script>
 
 <template>
   <el-row class="row-bg" justify="center" style="min-height: 750px">
     <el-col :span="16"><div class="grid-content ep-bg-purple" />
-      Main
+      <el-row>
+        <el-col :span="18">
+          <PostComponent/>
+        </el-col>
+        <el-col :span="6">
+        </el-col>
+      </el-row>
+
     </el-col>
   </el-row>
 </template>
