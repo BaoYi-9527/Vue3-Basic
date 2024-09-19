29754c4e1daf35be92c771a0d8ee59269d894dc1
weicheng <1648186705@qq.com>
2024-09-20 00:13:14 +0800

    feat:引入 icon & 完善 post card

--- src/components/Post/PostComponent.vue
+++ src/components/Post/PostComponent.vue
@@ -1,5 +1,6 @@
 <script setup>
   import logo from '@/assets/img/common/head.jpg'
+  import {Comment, Histogram, More, Share, Star} from "@element-plus/icons-vue";
 </script>
 
 <template>
@@ -11,7 +12,34 @@
       </div>
     </template>
     <div>凤凰非梧桐而不栖。且众木森森，胡有不可栖者，岂独梧桐乎?答日：夫凤凰仁瑞之禽也，不止强恶之木，梧桐，叶软之木也，皮理细腻而脆，枝干扶疏而软，故凤凰非梧而不栖也。</div>
-    <template #footer>阅读原文 -- 百度百科</template>
+    <template #footer>
+      <el-row justify="space-between">
+        <el-col :span="8">
+          <el-button round size="small">
+            <el-icon><Histogram /></el-icon>
+            <span> 987 </span>
+          </el-button>
+          <el-button round size="small">
+            <el-icon><Star /></el-icon>
+            <span> 188 </span>
+          </el-button>
+          <el-button round size="small">
+            <el-icon><Comment /></el-icon>
+            <span> 54 </span>
+          </el-button>
+        </el-col>
+        <el-col :span="4" style="display: flex; justify-content: flex-end;">
+          <el-button round size="small">
+            <el-icon><Share /></el-icon>
+            <span> 分享 </span>
+          </el-button>
+          <el-button round size="small">
+            <el-icon><More /></el-icon>
+            <span> 更多 </span>
+          </el-button>
+        </el-col>
+      </el-row>
+    </template>
   </el-card>
 </template>
 
